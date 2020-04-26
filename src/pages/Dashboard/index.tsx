d5daf35534fda45676563206c4e800d2c3b9267b
unknown <roger.mokarzel@gmail.com>
2020-04-25 21:55:48 -0300

    estilizando o dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,73 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositorios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositorios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositorio" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/17307453?s=460&u=d2be02a7c66664f177e4727d4b3bf2bd28d193fd&v=4"
+            alt="Roger Mokarzel"
+          />
+          <div>
+            <strong>rogermoka/nodejs</strong>
+            <p>NodeJS course</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/17307453?s=460&u=d2be02a7c66664f177e4727d4b3bf2bd28d193fd&v=4"
+            alt="Roger Mokarzel"
+          />
+          <div>
+            <strong>rogermoka/nodejs</strong>
+            <p>NodeJS course</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/17307453?s=460&u=d2be02a7c66664f177e4727d4b3bf2bd28d193fd&v=4"
+            alt="Roger Mokarzel"
+          />
+          <div>
+            <strong>rogermoka/nodejs</strong>
+            <p>NodeJS course</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/17307453?s=460&u=d2be02a7c66664f177e4727d4b3bf2bd28d193fd&v=4"
+            alt="Roger Mokarzel"
+          />
+          <div>
+            <strong>rogermoka/nodejs</strong>
+            <p>NodeJS course</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
